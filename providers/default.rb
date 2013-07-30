b8f5b6a527b8ef43d8791a290ec5760740ea0ca4
Andrew Gross <andrew@yipit.com>
2013-07-30 16:18:28 -0400

    fix logic for unmounted drives

--- providers/default.rb
+++ providers/default.rb
@@ -8,20 +8,6 @@ action :format do
   needs_formatting = needs_formatting(new_resource)
   mounted = mounted?(new_resource)
 
-  mount "Mount Device #{block_device} to #{mount_point}" do
-    action :nothing
-    mount_point mount_point
-    device block_device
-    options options
-  end
-
-  execute "Format Device #{block_device} to #{file_system}" do
-    action :nothing
-    command "mkfs.#{file_system} #{block_device}"
-    notifies :mount, "mount[Mount Device #{block_device} to #{mount_point}]", :immediately
-    notifies :enable, "mount[Mount Device #{block_device} to #{mount_point}]", :immediately
-  end
-
   mount "Unmount Block Device #{block_device}" do
     action :umount
     mount_point mount_point
@@ -29,6 +15,21 @@ action :format do
     only_if { needs_formatting && mounted }
     notifies :run, "execute[Format Device #{block_device} to #{file_system}]", :immediately
   end 
+
+  execute "Format Device #{block_device} to #{file_system}" do
+    action :run
+    command "mkfs.#{file_system} #{block_device}"
+    notifies :mount, "mount[Mount Device #{block_device} to #{mount_point}]", :immediately
+    notifies :enable, "mount[Mount Device #{block_device} to #{mount_point}]", :immediately
+    only_if { needs_formatting }
+  end
+
+  mount "Mount Device #{block_device} to #{mount_point}" do
+    action :nothing
+    mount_point mount_point
+    device block_device
+    options options
+  end
 
   new_resource.updated_by_last_action(true)
 
